190a999cef8ad534a5061fa3650400e01d59de9e
wheaton5 <whheaton@gmail.com>
2024-03-27 23:44:21 -0500

    nice ground truth comparison output, threshold on posteriors, but this seems a bit suspect, so might be rolled back

--- cellector.py
+++ cellector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from scipy.stats import betabinom
+import scipy.special
 import argparse
 from plotnine import *
 import numpy as np
@@ -16,8 +17,10 @@ parser.add_argument("--min_alt", required=False, default = 10, type=int, help="m
 parser.add_argument("--barcodes", required=True, help="barcodes.tsv file")
 parser.add_argument("--ground_truth", required=False, help="cell hashing assignments to barcodes or similar, two columns first column barcode, second column ground truth assignment")
 parser.add_argument("--output_prefix", required=True, help="output prefix")
+parser.add_argument("--min_alleles_posterior", required = False, default = 10, type = int, help="I wish I could be of more help here")
+parser.add_argument("--minority_prior", required = False, default = 0.1, type=float, help="prior used for posterior probability calculation in cell assignment, this could be the % you expect or just .5 to let the data speak for itself")
+parser.add_argument("--assignment_threshold", required = False, default=0.999, type=float, help="posterior probability threshold for cell assignment")
 args = parser.parse_args()
-
 if os.path.isdir(args.output_prefix):
     print("restarting pipeline in existing directory? not implemented yet " + args.output_prefix)
 else:
@@ -93,6 +96,8 @@ loci_used_sorted = sorted(list(loci_used))
 cells_to_exclude = set()
 iteration = 0
 any_change = True
+
+cell_posteriors = {} # map from cell id to [minority_posterior, majority_posterior]
 while any_change:
     any_change = False
     cell_log_likelihoods = {} # map from cell id to [log_likelihood, #loci_used, #alleles_used]
@@ -179,7 +184,7 @@ while any_change:
                     cells_to_exclude.add(cell_id)
         print("removed ",removed_cells," in the ",iteration," iteration")
         # lets find which loci contribute the most to exclusion
-        exclusion_locus_loglikes = {} #map from locus to sum of loglikelihoods from excluded cells
+        exclusion_locus_loglikes = {} # map from locus to sum of loglikelihoods from excluded cells
         exclusion_locus_cellcounts = {}
         exclusion_locus_alts = {}
         exclusion_locus_refs = {}
@@ -197,7 +202,7 @@ while any_change:
                 if locus in cell_counts:
                     cell_locus_counts =  cell_counts[locus]
                 else:
-                    cell_locus_counts = [0,0]
+                    cell_locus_counts = [0, 0]
                 num_alt = cell_locus_counts[1]
                 num_ref = cell_locus_counts[0]
                 if cell in cells_to_exclude:
@@ -210,6 +215,8 @@ while any_change:
                     majority_locus_refs[locus] += num_ref
         sorted_locus_loglikes = sorted(exclusion_locus_loglikes.items(), key=lambda x:x[1])
         outfile = filename[:-4]+"_locus_contribution_minority.tsv"
+        
+        log_likelihoods_for_post = {} # map from cell to [log_likelihood_minority, log_likelihood_majority]
         with open(outfile,'w') as out:
             out.write("\t".join(["locus","log_likelihood_minority","minority_cellcount", "log_likelihood_minority_per_cell","minority_alt","minority_ref","majority_alt","majority_ref","minority_af","majority_af"])+"\n")
             for (locus, loglike) in sorted_locus_loglikes:
@@ -221,25 +228,104 @@ while any_change:
                 minority_ref = exclusion_locus_refs[locus]
                 minority_af = 0
                 if minority_alt + minority_ref > 0:
-                    minority_af = minority_alt/(minority_alt+minority_ref)
+                    minority_af = minority_alt/(minority_alt + minority_ref)
                 majority_alt = majority_locus_alts[locus]
                 majority_ref = majority_locus_refs[locus]
                 majority_af = 0
                 if majority_alt + majority_ref > 0:
-                    majority_af = majority_alt/(majority_alt+majority_ref)
+                    majority_af = majority_alt/(majority_alt + majority_ref)
+                if minority_alt + minority_ref > args.min_alleles_posterior and majority_alt + majority_ref > args.min_alleles_posterior:
+                    for cell_id in cell_data.keys():
+                        cell_counts = cell_data[cell_id]
+                        cell_log_likelihoods_for_post = log_likelihoods_for_post.setdefault(cell_id, [0,0])
+                        if locus in cell_counts:
+                            cell_locus_counts =  cell_counts[locus]
+                        else:
+                            continue
+                        num_alt = cell_locus_counts[1]
+                        num_ref = cell_locus_counts[0]
+                        # betabinom.logpmf(num_alt, total_alleles, alpha, beta)
+                        if num_alt + num_ref > 0:
+                            cell_log_likelihoods_for_post[0] += betabinom.logpmf(num_alt, num_alt + num_ref, minority_alt + 1, minority_ref + 1)
+                            cell_log_likelihoods_for_post[1] += betabinom.logpmf(num_alt, num_alt + num_ref, majority_alt + 1, majority_ref + 1)
+                            #print(num_alt, num_ref, minority_alt, minority_ref, majority_alt, majority_ref, betabinom.logpmf(num_alt, num_alt + num_ref, minority_alt + 1, minority_ref + 1), betabinom.logpmf(num_alt, num_alt + num_ref, majority_alt + 1, majority_ref + 1))
+            
                 out.write("\t".join([str(locus), str(loglike), str(exclusion_locus_cellcounts[locus]), str(loglikepercell),str(minority_alt),str(minority_ref),str(majority_alt),str(majority_ref),str(minority_af),str(majority_af)])+"\n")
+            
+        priors = [args.minority_prior, 1-args.minority_prior]
+        log_priors = np.log(priors)
+        for cell_id in cell_data.keys():
+            cell_log_likelihoods_for_post = log_likelihoods_for_post[cell_id]
+            posts = []
+            marginals = []
+            for i in range(2):
+                marginals.append(cell_log_likelihoods_for_post[i] + log_priors[i])
+            denom = scipy.special.logsumexp(marginals)
+            for i in range(2):
+                posts.append(marginals[i] - denom)
+            cell_posteriors[cell_id] = np.exp(posts)
+            
+                
         iteration += 1
+assignment_gt_counts = {} # map from cellector assignment to gt assignment to counts
+gts = set()
+gt_counts = {}
 with open(args.output_prefix+"/cellector_assignments.tsv",'w') as out:
-    out.write("\t".join(["barcode","cellector_assignment","ground_truth_assignment"])+"\n")
+    out.write("\t".join(["barcode","cellector_assignment","minority_posterior", "majority_posterior","ground_truth_assignment"])+"\n")
     with open(args.barcodes) as infile:
         for line in infile:
             bc = line.strip()
             cell_id = barcode_to_cell_id[bc]
-            assignment = "majority"
-            if cell_id in cells_to_exclude:
-                assignment = "minority"
+            posteriors = cell_posteriors[cell_id]
+            if posteriors[0] > args.assignment_threshold:
+                assignment = "0"
+            elif posteriors[1] > args.assignment_threshold:
+                assignment = "1"
+            else:
+                assignment = "unassigned"
             gt = "na"
             if cell_id in cell_id_assignment:
                 gt = cell_id_assignment[cell_id]
-        
-            out.write("\t".join([bc,assignment,gt])+"\n")
+            assignment_counts = assignment_gt_counts.setdefault(assignment, {})
+            assignment_counts.setdefault(gt, 0)
+            assignment_counts[gt] += 1
+            gts.add(gt)
+            gt_counts.setdefault(gt, 0)
+            gt_counts[gt] += 1
+            out.write("\t".join([bc,assignment,str(posteriors[0]), str(posteriors[1]), gt])+"\n")
+first_header = "cellector assignment   "
+header_offset = len(first_header)
+xoffset = 3
+for gt in gts:
+    xoffset = max(xoffset, len(gt) + 3)
+gt_order = sorted(gt_counts, key=gt_counts.get, reverse=True)
+xoffset = max(xoffset, len(first_header)-2)
+print("cell assignments vs ground truth assignment")
+print(assignment_gt_counts)
+print("pretty print")
+print()
+
+header = "  "+"      ".join(["0","1","unassigned"])
+print(first_header + header)
+print("cell hashing"+" "*(xoffset-len("cell hashing")) + "|"+"-"*(len(header)+4)+"|")
+for gt in gt_order:
+    majority_count = "0"
+    minority_count = "0"
+    unassigned_count = "0"
+    if "0" in assignment_gt_counts:
+        count0 = assignment_gt_counts.get("0")
+        if gt in count0:
+            majority_count = str(count0.get(gt))
+    if "1" in assignment_gt_counts:
+        count1 = assignment_gt_counts.get("1")
+        if gt in count1:
+            minority_count = str(count1.get(gt))
+    if "unassigned" in assignment_gt_counts:
+        countun = assignment_gt_counts.get("unassigned")
+        if gt in countun:
+            unassigned_count = str(countun.get(gt))
+    string = gt + " "*(xoffset-len(gt)-1)+" |  " +majority_count+" "*(4-len(majority_count))+" |  "+minority_count+" "*(4-len(minority_count))+" |  "+unassigned_count+" "*(12-len(unassigned_count))+"|"
+    print(string)
+    #print(gt + " "*(xoffset-len(gt)-1)+" | " + "  |  ".join([majority_count, minority_count, unassigned_count]) + "    |")
+print(" "*xoffset+ "|"+"-"*(len(header)+4)+"|")
+
